83d2896cf7b98a8bc0b6295fd93b352decb35071
Narayan Sharma <narayansharma@ucreate.co.in>
2019-04-30 12:43:12 +0530

    configure entry point

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { port } = require('./app/config');
-const { registerAllRepos } = require('./app/repo/index');
-const { registerAllServices } = require('./app/services/index');
-const { bootstrapApplication } = require('./app/app');
-const { models } = require('./app/sequelize/models/index');
-const { registerAutoLoad } = require('./app/util/autoload');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { port, environment } = require("./app/config");
+const { registerAllRepos } = require("./app/repo/index");
+const { registerAllServices } = require("./app/services/index");
+const { bootstrapApplication } = require("./app/app");
+const { registerAutoLoad } = require("./app/util/autoload");
+const configuration = require("./knexfile")[environment];
+const knex = require("knex")(configuration); // eslint-disable-line import/order
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const repos = registerAllRepos({ models: models() });
+const repos = registerAllRepos({ knex });
 const services = {
   ...registerAllServices({ repos }),
-  registerAutoLoad: registerAutoLoad(),
+  registerAutoLoad: registerAutoLoad()
 };
 bootstrapApplication({ services, app });
 /*  eslint-disable-next-line no-console */
