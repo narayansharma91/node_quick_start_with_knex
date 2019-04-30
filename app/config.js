8e598cba8b3d67ce97ac759c9b6a0f8cbbff01a4
Narayan Sharma <narayansharma@ucreate.co.in>
2019-04-30 12:41:20 +0530

    change configurations

--- app/config.js
+++ app/config.js
@@ -4,12 +4,15 @@ const environment = process.env.NODE_ENV || 'development';
 const config = {
   development: {
     port: 4000,
+    environment,
   },
   production: {
     port: process.env.PORT || 4000,
+    environment,
   },
   test: {
     port: 5000,
+    environment,
   },
 };
 module.exports = config[environment];
