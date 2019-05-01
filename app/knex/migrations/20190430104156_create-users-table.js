12ffe4c75a123083af64bcfb584c4db86caab031
Narayan Sharma <narayansharma@ucreate.co.in>
2019-05-01 16:54:44 +0530

    Added PK & FK relation for users table.

--- app/knex/migrations/20190430104156_create-users-table.js
+++ app/knex/migrations/20190430104156_create-users-table.js
@@ -2,13 +2,14 @@
 exports.up = function(knex, Promise) {
      return knex.schema
     .createTable('users', function (table) {
-       table.increments('id');
+       table.increments('id').primary();
        table.string('firstName', 255).notNullable();
        table.string('lastName', 255).notNullable();
        table.string('email', 255).notNullable();
        table.integer('userTypeId').notNullable();
        table.date('createdAt', 255).notNullable();
        table.date('updatedAt', 255).notNullable();
+       table.foreign('userTypeId').references('id').inTable('user_types').onDelete('CASCADE');
     })
 };
 
